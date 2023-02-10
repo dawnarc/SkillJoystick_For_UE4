14a55c2794fbcb49440995960171bf04db44de9a
dawnarc <aiguo1024@gmail.com>
2023-02-10 18:53:33 +0800

    update to 4.27

--- Source/SkillJoystick/SkillJoystickCharacter.cpp
+++ Source/SkillJoystick/SkillJoystickCharacter.cpp
@@ -30,9 +30,9 @@ ASkillJoystickCharacter::ASkillJoystickCharacter()
 	// Create a camera boom...
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(RootComponent);
-	CameraBoom->bAbsoluteRotation = true; // Don't want arm to rotate when character does
+	CameraBoom->SetUsingAbsoluteRotation(true); // Don't want arm to rotate when character does
 	CameraBoom->TargetArmLength = 800.f;
-	CameraBoom->RelativeRotation = FRotator(-60.f, 0.f, 0.f);
+	CameraBoom->SetRelativeRotation(FRotator(-60.f, 0.f, 0.f));
 	CameraBoom->bDoCollisionTest = false; // Don't want to pull camera in when it collides with level
 
 	// Create a camera...
